ecb7dbdc03bbd841bdf80f4e66eb1ee384171a6c
Damien Young <123900384+dyventuresllc@users.noreply.github.com>
2024-06-30 18:42:23 -0700

    checking in final edits

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            string inputReversed = new string(inputArray);
+
+            return input.Equals(inputReversed);
+        }
+        
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kayak", true)] //Add test data <-------
+        [InlineData("mom", true)]
+        [InlineData("racecar", true)]
+        [InlineData("racecax", false)]
+        public void IsAPalindromeTest(string input, bool expected)
         {
+            //Start Step 3 here:
 
+            //Arrange
+            // create a Calculator object
+            var test = new WordSmith();
+
+            //Act
+            // call the Add method that is located in the Calculator class
+            // and store its result in a variable named actual
+            var actual = test.IsAPalindrome(input);    
+            
+            //Assert
+            //Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
